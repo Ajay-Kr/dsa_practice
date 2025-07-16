ac93d7808a9c08fa1994e0abf552083790123f0f
Ajay-Kr <ajaykumar1312.p@gmail.com>
2025-07-17 00:55:00 +0530

    Solved pattern problems (Pattern 05-06)

---
+++ striver_a2z_sheet/step_1_basics/LogicalThinkingProblems/07_Pattern_Equilateral_Triangle_Stars/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+
+    for(int i = 1; i <= n; i++) {
+      for(int j = n; j >= 1; j--) {
+        if(j > i) {
+          System.out.print("   ");
+        } else {
+          System.out.print("  *");
+        }
+      }
+      for(int j = 2; j <= n; j++) {
+        if(j <= i) {
+          System.out.print("  *");
+        }
+      }
+      System.out.println();
+    }
+
+    scanner.close();
+  }
+}
