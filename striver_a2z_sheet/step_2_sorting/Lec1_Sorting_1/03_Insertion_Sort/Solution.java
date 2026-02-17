e27f9475e7d2190041416483ff9b5b9c4b2a2e27
Ajay-Kr <ajaykumar1312.p@gmail.com>
2026-02-18 00:45:39 +0530

    added insertion sort

--- striver_a2z_sheet/step_2_sorting/Lec1_Sorting_1/03_Insertion_Sort/Solution.java
+++ striver_a2z_sheet/step_2_sorting/Lec1_Sorting_1/03_Insertion_Sort/Solution.java
@@ -1,3 +1,53 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Collections;
+
 public class Solution {
-  
+  static void printArray(List<Integer> arr) {
+    System.out.print("[");
+    for(int elm: arr) {
+      System.out.printf(" %d ", elm);
+    }
+    System.out.println("]");
+  }
+
+  static void insertionSort(List<Integer> arr) {
+    // INSERT ONE ELEMENT AT CORRECT PLACE AT A TIME
+    // TWO PARTS SORTED AND UNSORTED
+    // GET FIRST ELEMENT OF UNSORTED AND IT TO THE SORTED PART
+
+    for(int i = 1; i < arr.size(); i++) {
+      for(int j = i; j > 0; j--) {
+        if(arr.get(j-1) > arr.get(j)) {
+          Collections.swap(arr, j-1, j);
+        }
+      }
+    }
+  }
+  public static void main(String[] args) {
+     List<Integer> arr = new ArrayList<>();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter number of inputs: ");
+    int numOfElm = sc.nextInt();
+
+    if(numOfElm <= 0) {
+      System.out.println("Array size must be positive.");
+      sc.close();
+      return;
+    }
+
+    System.out.println("Enter array elements: ");
+    for(int i = 0; i < numOfElm; i++) {
+      int elm = sc.nextInt();
+      arr.add(elm);
+    }
+
+    System.out.println("input array: ");
+    printArray(arr);
+    insertionSort(arr);
+    System.out.println("sorted array: ");
+    printArray(arr);
+    sc.close();
+  }
 }
